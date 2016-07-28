b6492ee3be4d1f211ca3a56d997722ab4b61c3d0
劳一 <laoyi@laoyideMacBook-Pro.local>
2016-07-28 13:33:44 +0800

    code-搞定排列

--- practice/ViewController.m
+++ practice/ViewController.m
@@ -8,6 +8,7 @@
 
 #import "ViewController.h"
 #define ImageXY 50
+#define ImageCount 13
 
 @interface ViewController ()
 
@@ -19,22 +20,66 @@
     [super viewDidLoad];
     
     
-    int column = 3;
+    [self adjustwithimageCOL:3 get:YES];
+
+    
+   // [self setImage:@"1.png" x:35 y:133];
+ 
+}
+-(void)adjustwithimageCOL:(int)column get:(BOOL)add{
+    
     CGFloat TheY= 133;
     CGFloat gap = (self.view.frame.size.width - ImageXY * column)/(column +1);
     CGFloat TheX = gap;
     
-    for(int i =0 ;i<13;i++){
+    for(int i =0 ;i<ImageCount;++i){
         int no =i%8;
         NSString *png = [NSString stringWithFormat:@"%d.png",no+1 ];
-        CGFloat x = TheX + (i %column) *(gap+ImageXY);
+        CGFloat x = TheX +(i %column) *(gap+ImageXY);
         CGFloat y =TheY +(i/column) *(gap+ImageXY);
-           [self setImage:png x:x y:y];
-        NSLog(@"%.f,%.f",x,y);
+      //  NSLog(@"%.f,%.f",x,y);
+        if(add){
+        [self setImage:png x:x y:y];
+           // NSLog(@"%.f,%.f",x,y);
+           // NSLog(@"%@",self.class);
+            if(i == ImageCount-1){
+                CGFloat x = TheX +((i+1) %column) *(gap+ImageXY);
+                CGFloat y =TheY +((i+1)/column) *(gap+ImageXY);
+//                NSLog(@"%.f======,%.f",x,y);
+                [self setButtonwithBackgroundx:x y:y];
+                
+            }
+        }else{
+          //  NSLog(@"%i",i);
+            UIView *subview = self.view.subviews[i +1];
+        //    NSLog(@"%@",self.view.subviews[i+1]);
+            CGRect Fview= subview.frame;
+            Fview.origin = CGPointMake(x, y);
+//            NSLog(@"%.f,~~~~%.f",x,y);
+//            NSLog(@"%@",self.view.class);
+            
+            subview.frame =Fview;
+            if(i == ImageCount-1){
+                //[self setButtonwithBackgroundx:x y:y];
+               // NSLog(@"%i------",i);
+                UIView *subview = self.view.subviews[ImageCount+1];
+                //NSLog(@"%@",self.view.subviews[14]);
 
+                CGRect Fview= subview.frame;
+                CGFloat x = TheX +((i+1) %column) *(gap+ImageXY);
+                CGFloat y =TheY +((i+1)/column) *(gap+ImageXY);
+                Fview.origin = CGPointMake(x, y);
+               // NSLog(@"%.f,-----%.f",x,y);
+                subview.frame =Fview;
+            }
+
+
+        }
+//        NSLog(@"%i",i);
+
+      
     }
-   // [self setImage:@"1.png" x:35 y:133];
- 
+
 }
 -(void)setImage:(NSString *)icon x:(CGFloat)X y:(CGFloat)Y{
  
@@ -45,32 +90,30 @@
     [self.view addSubview:image];
 
 }
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
+-(void)setButtonwithBackgroundx:(CGFloat)x y:(CGFloat)y{
+    
+    UIButton *btn = [[UIButton alloc] init];
+    btn.frame = CGRectMake(x, y, ImageXY, ImageXY);
+    UIImage *b = [UIImage imageNamed:@"12.png"];
+ //   btn.backgroundColor = [UIColor colorWithPatternImage:b];
+    [btn setBackgroundImage:b forState:UIControlStateNormal];
+    [btn addTarget:self action:@selector(addImage) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:btn];
+}
+-(void)addImage{
+    NSLog(@"a a ");
+    UIImage *ima = [[UIImage alloc] init];
+    
+}
 - (IBAction)segment:(UISegmentedControl *)sender {
     [UIView beginAnimations:nil context:nil];
-    [UIView setAnimationDuration:0.77];
-    int column = sender.selectedSegmentIndex+ 3;
-    CGFloat TheY= 133;
-    CGFloat gap = (self.view.frame.size.width - ImageXY * column)/(column +1);
+    [UIView setAnimationDuration:0.33];
+   // int column = sender.selectedSegmentIndex+ 3;
+    [self adjustwithimageCOL:sender.selectedSegmentIndex + 3 get:NO];
+        
 
-    CGFloat TheX = gap;
     
-    for(int i =0 ;i<13;i++){
-        CGFloat x = TheX + (i %column) *(gap+ImageXY);
-        CGFloat y =TheY +(i/column) *(gap+ImageXY);
-         UIView *subview = self.view.subviews[i +1];
-        CGRect Fview= subview.frame;
-        
-        Fview.origin = CGPointMake(x, y);
-        
-        subview.frame =Fview;
-        
-
-    }
     [UIView commitAnimations];
 
 }
